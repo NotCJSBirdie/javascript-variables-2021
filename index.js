6fe439f1922c7ef13f7e7493da1e606c9424b650
Carl Serquiña <serquinajhunlo@gmail.com>
2021-10-18 10:22:12 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// This is an external JS //
+
+function myfunction() {
+  var x, y, total;
+  var x = 10;
+  var y = 11;
+  var total = x + y;
+  document.getElementById("Hello1P").innerHTML = total;
+}
+
+function myfunction1() {
+  var x = "Carl";
+  var y = "Serquina";
+  var z = x + y;
+
+  document.getElementById("Hello2P").innerHTML = x + "" + y;
+}
